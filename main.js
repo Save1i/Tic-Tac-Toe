e6b0ff9fb387ca6489812f96960421310191e361
Savelii Kosevich <savva0512@gmail.com>
2024-04-02 12:07:21 +0300

    ver 1.2

--- main.js
+++ main.js
@@ -4,9 +4,14 @@ const gamePage = document.querySelector(".game__page");
 const cells = document.querySelectorAll("#cell");
 const infoZone = document.querySelector("#infoZone");
 const infoZoneText = document.querySelector("#infoZoneText");
+const scoreOne = document.querySelector("#scoreOne span");
+const scoreTwo = document.querySelector("#scoreTwo span");
 
 let gameDeck = { 1: [0, 1, 2], 2: [3, 4, 5], 3: [6, 7, 8] };
 let firstPlayer = 0;
+
+let i = 0;
+let u = 0;
 
 function gameDeckObject(dataX, cell) {
   let x = parseInt(dataX);
@@ -35,8 +40,6 @@ function gameDeckObject(dataX, cell) {
   seeWhoWin();
 }
 
-// console.log(allStrings);
-
 function seeWhoWin() {
   let allStrings = Object.keys(gameDeck).every((key) =>
     gameDeck[key].every((value) => typeof value === "string")
@@ -56,6 +59,8 @@ function seeWhoWin() {
         (b[1][0] == "x" && b[2][1] == "x" && b[3][2] == "x") ||
         (b[1][2] == "x" && b[2][1] == "x" && b[3][0] == "x")
       ) {
+        i++;
+        scoreOne.textContent = `${i}`;
         console.log("First player Win!");
         firstPlayer = 0;
         gameDeck = { 1: [0, 1, 2], 2: [3, 4, 5], 3: [6, 7, 8] };
@@ -75,6 +80,8 @@ function seeWhoWin() {
         (b[1][0] == "o" && b[2][1] == "o" && b[3][2] == "o") ||
         (b[1][2] == "o" && b[2][1] == "o" && b[3][0] == "o")
       ) {
+        u++;
+        scoreTwo.textContent = `${u}`;
         console.log("Second player Win!");
         firstPlayer = 0;
         gameDeck = { 1: [0, 1, 2], 2: [3, 4, 5], 3: [6, 7, 8] };
@@ -108,12 +115,9 @@ function seeWhoWin() {
 
 seeWhoWin();
 
-// const gameDeck = { 1: ["0 0", "0 1", "0 2"], 2: ["1 0", "1 1", "1 2"], 3: ["2 0", "2 1", "2 2"] };
-
 const initCell = function () {
   cells.forEach((cell) => {
     cell.addEventListener("click", (e) => {
-      // console.log(e.target.dataset.x, e.target.dataset.y);
       console.log(cell);
       gameDeckObject(e.target.dataset.x, cell);
     });
